718101421a931902a70529f35b220257c6a92d5c
Arika <Delete0093@Gmail.com>
2018-08-18 01:34:49 +0900

    update script update

--- update.bat
+++ update.bat
@@ -1,11 +1,13 @@
 @echo off
 cd /d %~dp0
+set rep_user=Arika0093
+set rep_name=tootdeck
 echo -------------------------------------
 echo  tootdeck update program
 echo -------------------------------------
 ::Download
 echo git repositoryから最新版をダウンロードします…
-curl -L -O -# "https://github.com/Arika0093/tootdeck/archive/master.zip"
+curl -L -O -# "https://github.com/%rep_user%/%rep_name%/archive/master.zip"
 
 ::Unzip
 echo ダウンロードしたzipファイルを解凍します。
@@ -13,11 +15,13 @@ powershell Expand-Archive -Path master.zip -DestinationPath tmp/ -Force
 
 ::moved
 echo ダウンロードしたファイルを反映させます。
-xcopy /S /E /F /G /R /K /Y /V .\tmp\tootdeck-master\* %~dp0 /EXCLUDE:ignorelist
+echo update.bat > ignorelist
+xcopy /S /E /F /Q /G /R /K /Y /V .\tmp\%rep_name%-master\* %~dp0 /EXCLUDE:ignorelist
 
 ::filedelete
 echo 一時ファイルを削除します。
 del master.zip
+del ignorelist
 rmdir /s /q tmp\
 
 echo Update finish.
